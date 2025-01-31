2444f0d8c3d1486563f600630715b469e464f2c8
CPTforever <yester1324@gmail.com>
2025-01-31 10:31:49 +0000

    fixed more imports

--- src/twizzler.rs
+++ src/twizzler.rs
@@ -1,4 +1,6 @@
-pub fn get() -> io::Result<OsString> {
+use std::ffi::OsString;
+
+pub fn get() -> std::io::Result<OsString> {
     return Err(std::io::Error::new(
         std::io::ErrorKind::Unsupported,
         "Tell Ashley to complete this",
@@ -6,7 +8,7 @@ pub fn get() -> io::Result<OsString> {
 }
 
 #[cfg(feature = "set")]
-pub fn set(hostname: &OsStr) -> io::Result<()> {
+pub fn set(hostname: &OsStr) -> std::io::Result<()> {
     return Err(std::io::Error::new(
         std::io::ErrorKind::Unsupported,
         "Tell Ashley to complete this",
