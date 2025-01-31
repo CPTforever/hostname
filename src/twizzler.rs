512b8100125764b573f93a7913ac86a22afdcc70
CPTforever <yester1324@gmail.com>
2025-01-31 10:31:08 +0000

    fixed import

--- src/twizzler.rs
+++ src/twizzler.rs
@@ -1,14 +1,14 @@
 pub fn get() -> io::Result<OsString> {
-    return Err(Error::new(
-        ErrorKind::Unsupported,
+    return Err(std::io::Error::new(
+        std::io::ErrorKind::Unsupported,
         "Tell Ashley to complete this",
     )); 
 }
 
 #[cfg(feature = "set")]
 pub fn set(hostname: &OsStr) -> io::Result<()> {
-    return Err(Error::new(
-        ErrorKind::Unsupported,
+    return Err(std::io::Error::new(
+        std::io::ErrorKind::Unsupported,
         "Tell Ashley to complete this",
     )); 
 }
